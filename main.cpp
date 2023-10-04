0fcd5fb287353f49612060901b85f986e615ae29
Praveen-Yadav64 <139007042+Praveen-Yadav64@users.noreply.github.com>
2023-10-04 21:37:19 +0530

    This is the Main file for Calci App

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int Input_Number;
+cout << "Hi there Little Calci!\n";
+cout << "Input a Number : ";
+cin >> Input_Number;
+Input_Number=Input_Number+2;
+cout << "Your Number + 2 is : "<<Input_Number<<endl;
+return 0;
+
+}
