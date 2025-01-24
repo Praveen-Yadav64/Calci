1f651b23ae00e0d53728e43be2f541133422b4c9
Praveen-Yadav64 <139007042+Praveen-Yadav64@users.noreply.github.com>
2025-01-24 21:37:42 +0530

    Updated with Exception Handling and Menu Driven Operation

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-int Input_Number;
-cout << "Hi there Little Calci!\n";
-cout << "Input a Number : ";
-cin >> Input_Number;
-Input_Number=Input_Number+2;
-cout << "Your Number + 2 is : "<<Input_Number<<endl;
-return 0;
 
+int main() {
+	int choice = 0;
+	float num1=0, num2=0, Answer=0;
+	cout << "Enter your numbers : " << endl;
+	cin >> num1 >> num2 ;
+	cout << "\n\nNow enter your choice of operation : \n" << endl;
+	start:
+	cout << "\n\nMenu\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n\n" << endl;
+	cin >> choice ;
+		switch (choice) {
+		case 1: Answer = num1 + num2;
+			break;
+		case 2: Answer = num1 - num2;
+			break;
+		case 3: Answer = num1 * num2;
+			break;
+		case 4: 
+			if (num2 == 0){
+				cout << "\n\nDivision by Zero is not Possible! You will always get Infinity\n\n" << endl;
+				Answer = num1 / num2;
+			}
+			else {
+				Answer = num1 / num2;
+			}
+			break;
+
+		default: cout << "Invalid choice Please retry : " << endl;
+			goto start;
+			break;
+		}
+	cout << "The Answer is :" << Answer<<endl;
+	return 0;
 }
